991b6f81206ac1e93f47824bf8ab9c9f6af5f0eb
Zhenyu Gao <zhenyu.gao@irdeto.com>
2020-03-25 08:57:58 +0100

    lynx enn

---
+++ lynx/enn_lynx.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+df=pd.read_csv('lynx_train_X.csv', sep=' ',header=None)
+new_train_X = df.values
+df=pd.read_csv('lynx_train_Y.csv', sep=' ',header=None)
+new_train_Y = df.values
+
+X_train_inputs = []
+Y_train_outputs = []
+
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+
+# L = len(new_train_X)
+L = len(new_train_X)
+for i in range(L):
+    X_train_inputs.append(tuple(new_train_X[i]))
+for i in range(L):
+    Y_train_outputs.append(tuple(new_train_Y[i]))
+
+import multiprocessing
+import os
+import neat
+import visualize
+
+import warnings
+
+xor_inputs = X_train_inputs
+xor_outputs = Y_train_outputs
+
+def eval_genome(genome, config):
+    """
+    This function will be run in parallel by ParallelEvaluator.  It takes two
+    arguments (a single genome and the genome class configuration data) and
+    should return one float (that genome's fitness).
+
+    Note that this function needs to be in module scope for multiprocessing.Pool
+    (which is what ParallelEvaluator uses) to find it.  Because of this, make
+    sure you check for __main__ before executing any code (as we do here in the
+    last few lines in the file), otherwise you'll have made a fork bomb
+    instead of a neuroevolution demo. :)
+    """
+
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+    error = 0.0
+    for xi, xo in zip(xor_inputs, xor_outputs):
+        output = net.activate(xi)
+        error -= (output[0] - xo[0]) ** 2
+    mse = error/L
+    return mse
+
+
+def run(config_file):
+    # Load configuration.
+    config = neat.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_file,
+    )
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    # Run for up to 300 generations.
+    pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), eval_genome)
+    winner = p.run(pe.evaluate, 10000)
+
+    # Display the winning genome.
+    print("\nBest genome:\n{!s}".format(winner))
+
+    # Show output of the most fit genome against training data.
+    print("\nOutput:")
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    for xi, xo in zip(xor_inputs, xor_outputs):
+        output = winner_net.activate(xi)
+        print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
+    
+    
+    df=pd.read_csv('lynx_test_X.csv', sep=' ',header=None)
+    new_test_X = df.values
+    X_test_inputs = []
+    for i in range(len(new_test_X)):
+        X_test_inputs.append(tuple(new_test_X[i]))
+
+    predictions_enn = []
+    for xi in X_test_inputs:
+        output = winner_net.activate(xi)
+        predictions_enn.append(output)
+
+    np.savetxt('predictions_enn.csv', np.array(predictions_enn), delimiter=',')
+
+    node_names = {-1: "-7", -2: "-6", -3: "-5", -4:"-4", -5:"-3", -6:"-2", -7:"-1",0: "Target"}
+    visualize.draw_net(config, winner, True, node_names=node_names)
+    visualize.plot_stats(stats, ylog=False, view=True)
+    visualize.plot_species(stats, view=True)
+
+
+
+
+
+if __name__ == "__main__":
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.getcwd()
+    config_path = os.path.join(local_dir, "config-feedforward")
+    run(config_path)
