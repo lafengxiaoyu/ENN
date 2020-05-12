17d7cbee590047b5b9f96ff7d71f22aadddac6bf
zhenyu.gao <zhenyu.gao@irdeto.com>
2020-05-12 23:02:27 +0200

    add plot graph

--- mnist/enn_mnist10softmax.py
+++ mnist/enn_mnist10softmax.py
@@ -152,3 +152,190 @@ visualize.draw_net(config, winner, True, fmt="png")
 visualize.plot_stats(stats, ylog=False, view=False)
 visualize.plot_species(stats, view=False)
 
+# program to check if there is exist a path between two vertices 
+# of a graph 
+
+from collections import defaultdict 
+
+#This class represents a directed graph using adjacency list representation 
+class Graph: 
+
+	def __init__(self,vertices): 
+		self.V= vertices #No. of vertices 
+		self.graph = defaultdict(list) # default dictionary to store graph 
+
+	# function to add an edge to graph 
+	def addEdge(self,u,v): 
+		self.graph[u].append(v) 
+	
+	# Use BFS to check path between s and d 
+	def isReachable(self, s, d): 
+		# Mark all the vertices as not visited 
+		visited =[False]*(self.V) 
+
+		# Create a queue for BFS 
+		queue=[] 
+
+		# Mark the source node as visited and enqueue it 
+		queue.append(s) 
+		visited[s] = True
+
+		while queue: 
+
+			#Dequeue a vertex from queue 
+			n = queue.pop(0) 
+			
+			# If this adjacent node is the destination node, 
+			# then return true 
+			if n == d: 
+				return True
+
+			# Else, continue to do BFS 
+			for i in self.graph[n]: 
+				if visited[i] == False: 
+					queue.append(i) 
+					visited[i] = True
+		# If BFS is complete without visited d 
+		return False
+    
+    
+def findAllPath(graph,start,end,path=[]):
+    path = path +[start]
+    if start == end:
+        return [path]
+ 
+    paths = [] #存储所有路径    
+    for node in graph[start]:
+        if node not in path:
+            newpaths = findAllPath(graph,node,end,path) 
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+used_nodes = list(winner.nodes.keys())
+# create a dict for mapping 
+l1 = numbers + config.genome_config.input_keys + used_nodes[number_of_classification:]
+l2 = range(len(l1))
+dict_nodes = dict(zip(l1, l2))
+
+### 将节点加入图
+g = Graph(len(l1))# inputs + outputs + used  
+# add connections in the graph
+for cg in winner.connections.values():
+    if cg.enabled:
+        g.addEdge(dict_nodes[cg.key[0]],dict_nodes[cg.key[1]])
+        
+v = numbers
+list_connections = []
+for vi in v:
+    for u in range(number_of_classification, len(config.genome_config.input_keys) + number_of_classification):
+        list_connections.append(g.isReachable(u, vi))
+
+nodes_inputs = range(number_of_classification, number_of_classification+ 784)
+nodes_outputs = range(number_of_classification)
+nodes_mid = range(number_of_classification + 784, len(dict_nodes))
+
+# 找到所有输入和输出连接的路
+all_path = []
+for u in nodes_inputs:
+    for v in nodes_outputs:
+        path = findAllPath(g.graph, u, v)
+        if path:
+            all_path = all_path + path
+
+# 得到最长路，以及每个节点在哪个层级
+max_length = max([len(x) for x in all_path])
+nodes_tuples_list = []
+for path in all_path:
+    #print(path)
+    for node in path:
+        nodes_tuples_list.append([node, path.index(node)])
+
+# 确定节点的层级（消除重复）       
+nodes_tuples_fixed_list = []
+for index in range(max_length):
+    for nodes in nodes_tuples_list:
+        node, node_index = nodes[0], nodes[1]
+        if node_index == index: 
+            if nodes not in nodes_tuples_fixed_list:
+                nodes_tuples_fixed_list.append(nodes)
+
+    
+# 所有有连接的节点
+all_used_nodes = []
+for node in nodes_tuples_fixed_list:
+    if node[0] not in all_used_nodes:
+        all_used_nodes.append(node[0])
+
+# 确定节点的层级（最终确定）
+nodes_tuples_fixed_list_final = []
+
+for node in all_used_nodes:
+    if node in nodes_inputs:
+        nodes_tuples_fixed_list_final.append([node, 0])
+    elif node in nodes_outputs:
+        nodes_tuples_fixed_list_final.append([node, max_length - 1])
+    else:
+        list_node = []
+        for nodes in nodes_tuples_fixed_list:
+            if node == nodes[0]:
+                list_node.append(nodes)
+        layer = 0
+        for ele in list_node:
+            # 删除重复项，取最大
+            if ele[1] > layer:
+                layer = ele[1]
+        nodes_tuples_fixed_list_final.append([node,layer])
+
+
+# 每个层级的节点合数
+length_of_layers = []
+number_of_nodes = 0
+for index in range(max_length):
+    for nodes in nodes_tuples_fixed_list_final:
+        node, node_index = nodes[0], nodes[1]
+        if node_index == index: 
+            number_of_nodes += 1
+    length_of_layers.append(number_of_nodes)
+
+length_of_each_layer = []
+length_of_each_layer.append(length_of_layers[0])
+for i in range(1, max_length):
+    length_of_each_layer.append(length_of_layers[i] - length_of_layers[i-1])
+
+# 输出每个层级的节点个数
+print("length of each layers:", length_of_each_layer)
+
+# 所有端到端的路
+all_path_side2side = []
+for path in all_path:
+    if len(path) == 2:
+        all_path_side2side.append(path)
+    else:
+        for i in range(len(path)-1):
+            all_path_side2side.append([path[i],path[i+1]])
+
+# 定义节点到绘图的字典
+dict_nodes_graph = dict()
+count_number_layer = [0] * max_length
+for nodes in nodes_tuples_fixed_list_final:
+    node, layer = nodes
+    dict_nodes_graph[node] = [count_number_layer[layer], layer]
+    count_number_layer[layer] += 1
+
+max_nodes_one_layer = np.max(length_of_each_layer)
+for path in all_path_side2side:
+    node1, node2 = path
+    x1, y1 = dict_nodes_graph[node1]
+    x2, y2 = dict_nodes_graph[node2]
+    
+    interval1 = max_nodes_one_layer/length_of_each_layer[y1]
+    interval2 = max_nodes_one_layer/length_of_each_layer[y2]
+    
+    plt.plot([x1*interval1 + interval1/2, x2*interval2 + interval2/2], [y1, y2], linewidth=0.4)
+
+for layer in range(max_length):
+    interval = max_nodes_one_layer/length_of_each_layer[layer]
+    plt.scatter((interval * np.arange(length_of_each_layer[layer]))+ interval/2, [layer]*length_of_each_layer[layer])
+
+plt.savefig("nodes.png")
